d398eccf29b38442d9cc476fb13674b80fedb22a
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-04-08 18:50:37 +0800

    Sat Apr  8 06:50:37 PM CST 2023

--- build/cron.py
+++ build/cron.py
@@ -1,5 +1,9 @@
 from crontab import CronTab
-cron = CronTab(user='root')
-job = cron.new(command='echo `date` >> /tmp/d/date.txt')
-job.minute.every(1)
-cron.write()
+# cron = CronTab(user='root')
+# job = cron.new(command='echo `date` >> /tmp/d/date.txt')
+# job.minute.every(1)
+# cron.write()
+
+mem_cron = CronTab(tab="""
+  * * * * * echo `date` >> /tmp/d/date.txt
+""")
