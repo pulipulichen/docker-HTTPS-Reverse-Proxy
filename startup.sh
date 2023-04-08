1aac8e12638085a34c137fa0a425318af2769681
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-04-08 16:35:16 +0800

    Sat Apr  8 04:35:16 PM CST 2023

--- startup.sh
+++ startup.sh
@@ -4,11 +4,11 @@ if [ ! -f ./docker-compose.yml ]; then
   cp ./docker-compose.sample.yml ./docker-compose.yml
 fi
 
-if [ ! -f ./config/nginx/nginx-http.conf ]; then
+if [ ! -f ./conf/nginx/nginx-http.conf ]; then
   cp ./conf/nginx/nginx-http.sample.conf ./config/nginx/nginx-http.conf
 fi
 
-if [ ! -f ./config/nginx/nginx-https.conf ]; then
+if [ ! -f ./conf/nginx/nginx-https.conf ]; then
   cp ./conf/nginx/nginx-https.sample.conf ./config/nginx/nginx-https.conf
 fi
 
