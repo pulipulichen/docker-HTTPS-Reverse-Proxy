5445eb6c850b02ec19d96fc7ea0a6aabf3b288be
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-04-08 19:25:03 +0800

    Sat Apr  8 07:25:02 PM CST 2023

--- build/cron-certbot.sh
+++ build/cron-certbot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#while :
-#do
-  #sleep 2592000
-  sleep 10
+while :
+do
+  sleep 2592000
+  #sleep 10
 
   # 先把nginx環境準備好
   cp -f /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup
@@ -11,8 +11,9 @@
   /etc/init.d/nginx reload
 
   echo "certbot update..."
-  /usr/bin/certbot renew --dry-run
+  #/usr/bin/certbot renew --dry-run
+  /usr/bin/certbot renew
 
   cp -f /etc/nginx/nginx.conf.backup /etc/nginx/nginx.conf
   /etc/init.d/nginx reload
-#done
+done
