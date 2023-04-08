ede90c318c609196ca39ae656906b08dc34a1d28
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-04-08 12:31:58 +0800

    Sat Apr  8 12:31:58 PM CST 2023

--- build/setup-nginx-config.py
+++ build/setup-nginx-config.py
@@ -9,11 +9,11 @@ RP_BACKEND = os.environ.get('RP_BACKEND')
 # ===========
 
 try:
-  RP_DOMAIN_NAME = os.environ.get('RP_DOMAIN_NAME')
+  RP_DOMAIN_NAME = os.environ.get('CERTBOT_DOMAIN')
   #print('ENV environment variable exists')
   # print('okkkkk nooooo', RP_DOMAIN_NAME)
   if RP_DOMAIN_NAME == None:
-    raise Exception("RP_DOMAIN_NAME is not set")
+    raise Exception("CERTBOT_DOMAIN is not set")
   
   # sed -e 's/${RP_DOMAIN}/$CERTBOT_DOMAIN/g' -e 's/${RP_BACKEND}/$RP_BACKEND/g' /etc/nginx/nginx-https-template.conf  >> /etc/nginx/nginx.conf
 
